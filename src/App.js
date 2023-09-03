10a6c6cd7906dce4869d0f0429b1d774075b8b6d
Damien Legg <leggdamien@gmail.com>
2023-09-03 13:57:35 -0500

    Start of Pokedex exercise

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <Pokedex />
+    <Pokecard />
+    </div>
+  );
+}
+
+export default App;
